060bce6121eaaaf6cb6aa020d19c534e9e817c5d
saseivanovski <sase.ivanovski@gmail.com>
2022-07-04 17:51:01 +0200

    new exercise

--- codewars.js
+++ codewars.js
@@ -18,15 +18,24 @@ function DNAStrand(dna){
   return newarr
 }
 
+
 // return the highest and the lowest number in a string, with space seperated numbers
 function highAndLow(numbers) {
   numbers = numbers.split(" ");
   return Math.max(...numbers) + " " +  Math.min(...numbers)
 }
 
+
 //Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
 //(In this case, all triangles must have surface greater than 0 to be accepted).
-function isTriangle(a,b,c)
-{
+function isTriangle(a,b,c) {
   return (a + b > c) && (a + c > b) && (b + c > a);
 }
+
+
+//Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements
+function moveZeros(arr) {
+    return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
+  }
+
+  
