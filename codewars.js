b60fcb3c3e5aee2c63f49ad6b3469e6105011c35
saseivanovski <sase.ivanovski@gmail.com>
2022-07-05 14:30:05 +0200

    new exercise added

--- codewars.js
+++ codewars.js
@@ -1,17 +1,17 @@
-// return the counterpart of DNA for T --> A , C --> G and vise versa
+// 1. return the counterpart of DNA for T --> A , C --> G and vise versa
 function DNAStrand(dna){
-  let arr = dna.split("")
-  console.log(arr)
-  let newarr = []
+  let arr = dna.split("");
+  console.log(arr);
+  let newarr = [];
   for(var i=0;i<arr.length;i++){
     if(arr[i] == "A"){
-      newarr.push("T")
+      newarr.push("T");
     } else if (arr[i] == "T"){
-      newarr.push("A")
+      newarr.push("A");
     } else if (arr[i] == "C"){
-      newarr.push("G")
+      newarr.push("G");
     } else if (arr[i] == "G"){
-      newarr.push("C")
+      newarr.push("C");
     }
   }
   console.log(newarr)
@@ -19,39 +19,49 @@ function DNAStrand(dna){
 }
 
 
-// return the highest and the lowest number in a string, with space seperated numbers
+// 2. return the highest and the lowest number in a string, with space seperated numbers
 function highAndLow(numbers) {
   numbers = numbers.split(" ");
-  return Math.max(...numbers) + " " +  Math.min(...numbers)
+  return Math.max(...numbers) + " " +  Math.min(...numbers);
 }
 
 
-//Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+// 3. Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
 //(In this case, all triangles must have surface greater than 0 to be accepted).
 function isTriangle(a,b,c) {
   return (a + b > c) && (a + c > b) && (b + c > a);
 }
 
 
-//Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements
+// 4. Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements
 function moveZeros(arr) {
     return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
   }
 //OR like this
   function moveZeros(arr) {
-
-    var notZero = []
-    var zero = []
+    var notZero = [];
+    var zero = [];
   
     for(var i = 0 ; i < arr.length ; i++) {
       if(arr[i] == 0) { 
-        zero.push(0)
+        zero.push(0);
       } else {
-        notZero.push(arr[i])
+        notZero.push(arr[i]);
       }
     }
   
     return notZero.concat(zero)
   }
 
+
+  // 5. Second argument (tail), is the same as the last letter of the first argument (body). If the tail is right return true, else return false. The arguments will always be strings, and normal letters.
+  function correctTail(body, tail) {
+    let sub = body.substr(body.length-(tail.length));
+    
+    if (sub == tail) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   
